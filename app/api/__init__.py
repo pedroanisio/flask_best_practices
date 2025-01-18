480a4ed0c5136234f1fdc05813093421169365c5
Pedro Silva <pedroanisio@arc4d3.com>
2025-01-18 17:43:11 +0000

    feat: translate comments and documentation to English in __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -19,29 +19,27 @@ restful_api = Blueprint('api', __name__)
 api = Api(restful_api)
 
 """
-flask restful 路由注册
-带参数url 可以一起注册
-无参数：http://0.0.0.0:9999/api/demo
-带参数：http://0.0.0.0:9999/api/demo/123/456
-api.add_resource(DemoApi, '/demo', '/demo/<page>/<size>', endpoint='demo')
+Flask RESTful route registration
+Registering URL with and without parameters
+Without parameters: http://0.0.0.0:9999/api/demo
+With parameters: http://0.0.0.0:9999/api/demo/123/456
 """
 api.add_resource(DemoApi, '/demo', '/demo/<page>/<size>', endpoint='demo')
 api.add_resource(RestfulDemoApi, '/', endpoint='restful_demo_api')
 api.init_app(restful_api)
 
 """
-Method View 类视图路由注册
-带参数 url 需要分开注册
-无参数: http://0.0.0.0:9999/cms/demo
-带参数: http://0.0.0.0:9999/cms/demo/999/888
+Method View class-based route registration
+URLs with parameters need to be registered separately
+Without parameters: http://0.0.0.0:9999/cms/demo
+With parameters: http://0.0.0.0:9999/cms/demo/999/888
 """
-
 method_view_api.add_url_rule('/', view_func=MethodViewDemo.as_view('demo_get'))
 method_view_api.add_url_rule('/demo', view_func=MethodViewDemo.as_view('demo_post'))
-method_view_api.add_url_rule('/demo/<page>/<size>/', view_func=MethodViewDemo.as_view('demo_pram'))
+method_view_api.add_url_rule('/demo/<page>/<size>/', view_func=MethodViewDemo.as_view('demo_param'))
 
 """
-获取请求参数例子(与上述的flask_restful一致,这里只用 MethodView 作为例子)
+Examples of obtaining request parameters (similar to Flask RESTful, shown here using MethodView)
 """
 method_view_api.add_url_rule('/mv_params', view_func=MethodViewParams.as_view('mv_params'))
 method_view_api.add_url_rule('/mv_json', view_func=MethodViewJson.as_view('mv_json'))
@@ -49,30 +47,18 @@ method_view_api.add_url_rule('/mv_form_data', view_func=MethodViewFormData.as_vi
 method_view_api.add_url_rule('/mv_bytes_data', view_func=MethodViewBytesData.as_view('mv_bytes_data'))
 
 """
-路由注册
-
-@method_view_api.route('/', methods=["GET", "POST"])
-def index():
-    return jsonify('this cms')
-
-上面方式等价于以下方式(统一管理路由):
-
-def index():
-    return jsonify('this cms')
-    
-route_admin.add_url_rule('/index', methods=["GET", "POST"], endpoint='index', view_func=index)
-
+Route registration
 """
 method_view_api.add_url_rule('/m1', methods=["GET", "POST"], endpoint='module_01_index', view_func=module_01_index)
 
 """
-静态文件处理/访问方式
+Static file handling/access methods
 http://0.0.0.0:9999/static/flask.jpg
 http://0.0.0.0:9999/static/images/flask.jpg
 """
 
 """
-测试全局异常
+Testing global exceptions
 """
 api.add_resource(TestRestful, '/test_restful_ex', endpoint='test_restful_ex')
 method_view_api.add_url_rule('/test_mv_ex', view_func=TestMethodView.as_view('test_mv_ex'))
