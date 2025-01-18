de5eab4dc1f5f163f2f3342698795acf7b1831d6
Pedro Silva <pedroanisio@arc4d3.com>
2025-01-18 17:31:34 +0000

    feat: translate comments and HTML headings to English in models and templates

--- app/models/admin/models.py
+++ app/models/admin/models.py
@@ -11,32 +11,31 @@ from common.libs.BaseModel import *
 
 """
 RBAC
-用户-角色-权限
+User - Role - Permission
 """
 
 crm_user_and_role = db.Table(
     'cms_user_and_role',
-    db.Column('cms_user_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_user.id'), primary_key=True, comment='用户id'),
-    db.Column('cms_role_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_role.id'), primary_key=True, comment='角色id'),
-    comment='用户_角色_中间表'
+    db.Column('cms_user_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_user.id'), primary_key=True, comment='User ID'),
+    db.Column('cms_role_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_role.id'), primary_key=True, comment='Role ID'),
+    comment='User_Role_Mapping_Table'
 )
 
 crm_permission_and_role = db.Table(
     'cms_permission_and_role',
-    db.Column('cms_permission_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_permission.id'), primary_key=True,
-              comment='权限id'),
-    db.Column('cms_role_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_role.id'), primary_key=True, comment='角色id'),
-    comment='权限_角色_中间表'
+    db.Column('cms_permission_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_permission.id'), primary_key=True, comment='Permission ID'),
+    db.Column('cms_role_id', BIGINT(20, unsigned=True), db.ForeignKey('crm_role.id'), primary_key=True, comment='Role ID'),
+    comment='Permission_Role_Mapping_Table'
 )
 
 
 class Admin(BaseModel):
     __tablename__ = 'crm_user'
-    __table_args__ = {'comment': '后台用户表'}
-    username = db.Column(db.String(50), nullable=False, comment='账号')
-    _password = db.Column(db.String(100), nullable=False, comment='密码')
-    mail = db.Column(db.String(100), nullable=True, comment='邮箱')
-    remark = db.Column(db.String(255), nullable=True, comment='备注')
+    __table_args__ = {'comment': 'Admin User Table'}
+    username = db.Column(db.String(50), nullable=False, comment='Username')
+    _password = db.Column(db.String(100), nullable=False, comment='Password')
+    mail = db.Column(db.String(100), nullable=True, comment='Email')
+    remark = db.Column(db.String(255), nullable=True, comment='Remarks')
 
     def __init__(self, username, password):
         self.username = username
@@ -55,44 +54,42 @@ class Admin(BaseModel):
         return result
 
     def get_role(self):
-        """获取当前用户的所有角色"""
+        """Get all roles of the current user"""
         roles = self.roles
-        # print(roles)
         roles_json = [r.to_json() for r in roles]
         return roles_json
 
     def get_permission(self):
-        """获取当前用户的所有权限"""
+        """Get all permissions of the current user"""
         roles = self.roles
         permission_set = []
         for r in roles:
             permission_set += r.permission_list
-        # print(list(set(permission_set)))
         permission_json = [p.to_json() for p in list(set(permission_set))]
         return permission_json
 
     def __repr__(self):
-        return 'Admin 模型对象-> ID:{} 用户名:{}'.format(self.id, self.username)
+        return 'Admin Model Object-> ID:{} Username:{}'.format(self.id, self.username)
 
 
 class Role(BaseModel):
     __tablename__ = 'crm_role'
-    __table_args__ = {'comment': '后台角色表'}
-    name = db.Column(db.String(50), nullable=False, comment='角色名称')
-    remark = db.Column(db.String(255), nullable=True, comment='备注')
+    __table_args__ = {'comment': 'Admin Role Table'}
+    name = db.Column(db.String(50), nullable=False, comment='Role Name')
+    remark = db.Column(db.String(255), nullable=True, comment='Remarks')
 
     user_list = db.relationship('Admin', secondary=crm_user_and_role, backref='roles')
     permission_list = db.relationship('Permission', secondary=crm_permission_and_role, backref='roles')
 
     def __repr__(self):
-        return 'Role 模型对象-> ID:{} 角色名称:{}'.format(self.id, self.name)
+        return 'Role Model Object-> ID:{} Role Name:{}'.format(self.id, self.name)
 
 
 class Permission(BaseModel):
     __tablename__ = 'crm_permission'
-    __table_args__ = {'comment': '后台权限表'}
-    name = db.Column(db.String(50), nullable=False, comment='权限名称')
-    remark = db.Column(db.String(255), nullable=True, comment='备注')
+    __table_args__ = {'comment': 'Admin Permission Table'}
+    name = db.Column(db.String(50), nullable=False, comment='Permission Name')
+    remark = db.Column(db.String(255), nullable=True, comment='Remarks')
 
     def __repr__(self):
-        return 'Permission 模型对象-> ID:{} 权限名称:{}'.format(self.id, self.name)
+        return 'Permission Model Object-> ID:{} Permission Name:{}'.format(self.id, self.name)
