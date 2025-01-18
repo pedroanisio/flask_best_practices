48b9e955920a2a6d8b0891892b2686de647b16eb
Pedro Silva <pedroanisio@arc4d3.com>
2025-01-18 16:55:22 +0000

    feat: update comments and documentation to English; modify author information

--- ExtendRegister/command_register.py
+++ ExtendRegister/command_register.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/5/29 下午5:47
-# @Author  : ShaHeTop-Almighty-ares
-# @Email   : yang6333yyx@126.com
+# @Time    : 2024/01/18 4:02 PM
+# @Author  : Pedro Anisio Silva
+# @Email   : pedroanisio@arc4d3.com
 # @File    : command_register.py
-# @Software: PyCharm
 
 import os
 import click
@@ -17,51 +16,50 @@ from app.models.admin.models import Admin, Role, Permission
 export FLASK_APP=ApplicationExample.py
 """
 
-
 def register_commands(app):
-    """flask cli"""
+    """Flask CLI Commands"""
 
-    @app.cli.command("hello_world", help='hello-world')
+    @app.cli.command("hello_world", help='Hello World')
     def hello_world():
-        print('hello world')
+        print('Hello World')
 
-    @app.cli.command(help='首次进行ORM操作')
+    @app.cli.command(help='Initial ORM operations')
     def orm():
 
         migrations_path = os.getcwd() + "/migrations"
 
         if os.path.exists(migrations_path):
             shutil.rmtree(migrations_path)
-            print(f'>>> migrations 删除: {migrations_path}')
+            print(f'>>> Migrations deleted: {migrations_path}')
         else:
-            print(f'>>> migrations 不存在: {migrations_path}')
+            print(f'>>> Migrations not found: {migrations_path}')
 
         try:
             drop_table_sql = """DROP TABLE IF EXISTS alembic_version;"""
             print(drop_table_sql)
             drop_result = project_db.execute_sql(sql=drop_table_sql)
-            print(f'>>> drop_result: {drop_result}')
+            print(f'>>> Drop result: {drop_result}')
         except BaseException as e:
-            print(f'>>> 删除 alembic_version 失败:{e}')
+            print(f'>>> Failed to delete alembic_version: {e}')
 
         try:
             os.system("flask db init")
             os.system("flask db migrate")
             os.system("flask db upgrade")
-            print('>>> 创建成功')
+            print('>>> Creation successful')
         except BaseException as e:
-            print(f'>>> 创建失败:{e}')
+            print(f'>>> Creation failed: {e}')
 
-    @app.cli.command(help='更新表')
+    @app.cli.command(help='Update database tables')
     def table():
         try:
             os.system("flask db migrate")
             os.system("flask db upgrade")
-            print('>>> 创建成功')
+            print('>>> Update successful')
         except BaseException as e:
-            print(f'>>> 创建失败:{e}')
+            print(f'>>> Update failed: {e}')
 
-    @app.cli.command(help='创建用户角色权限')
+    @app.cli.command(help='Create user roles and permissions')
     def crm():
         try:
             user = Admin.query.filter_by(username='admin').first()
@@ -71,34 +69,34 @@ def register_commands(app):
                 user = Admin(username='admin', password='123456')
                 db.session.add(user)
                 db.session.commit()
-                print('cms用户添加成功')
+                print('CMS user added successfully')
             for r in range(1, 5):
-                role_obj = Role(name='角色{}'.format(str(r)))
+                role_obj = Role(name=f'Role {r}')
                 db.session.add(role_obj)
             db.session.commit()
-            print('cms角色添加成功')
+            print('CMS roles added successfully')
 
             for p in range(1, 5):
-                permission_obj = Permission(name='权限{}'.format(str(p)))
+                permission_obj = Permission(name=f'Permission {p}')
                 db.session.add(permission_obj)
             db.session.commit()
-            print('cms权限添加成功')
+            print('CMS permissions added successfully')
         except BaseException as e:
-            print('出错:{}'.format(str(e)))
+            print(f'Error: {e}')
 
-    @app.cli.command("create_user", help="创建用户")
-    @click.option("--username", help="用户名", type=str)
-    @click.option("--password", help="密码", type=str)
+    @app.cli.command("create_user", help="Create a user")
+    @click.option("--username", help="Username", type=str)
+    @click.option("--password", help="Password", type=str)
     def create_user(username, password):
         """
-        command: flask create-user --username yyx --password 123456
+        Command: flask create-user --username yyx --password 123456
         """
-
+        
         query_user = Admin.query.filter_by(username=username).first()
         if query_user:
-            print(f'>>> 用户: {username} 已存在')
+            print(f'>>> User: {username} already exists')
         else:
             user = Admin(username=username, password=password)
             db.session.add(user)
             db.session.commit()
-            print(f'用户: {username} 添加成功')
+            print(f'User: {username} added successfully')
